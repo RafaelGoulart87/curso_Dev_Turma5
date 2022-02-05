c08459d7035111840dcf2f8d645c4619dce32f7d
Rafael Goulart <goulart.rafaelrosa89@gmail.com>
2022-02-05 08:18:06 -0300

    class week1 mounth2

--- Month2/week1/Class/projectWeek5/main.js
+++ Month2/week1/Class/projectWeek5/main.js
@@ -1,6 +1,7 @@
 import { readCSV, formatCSV } from "./csvHelpers.js"
 import { csvToJson, saveToJason } from "./jsonHelpers.js"
 
+
 const main = () => {
     const csvReaded = readCSV("listProducts.csv")
     const csvFormated = formatCSV(csvReaded)
